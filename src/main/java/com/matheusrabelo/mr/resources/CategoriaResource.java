69d630f2e70925e4c430301d631ffaa0f55dbb84
Matheus rabelo <matheusrabelo520@gmail.com>
2021-02-17 19:54:24 -0300

    Testando a primeira classe de dominio

--- src/main/java/com/matheusrabelo/mr/resources/CategoriaResource.java
+++ src/main/java/com/matheusrabelo/mr/resources/CategoriaResource.java
@@ -1,18 +1,29 @@
 package com.matheusrabelo.mr.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.matheusrabelo.mr.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categorias")
-// ele irá responder por esse endereço  (value="/categorias")
 public class CategoriaResource {
 
-	// ira obter um dado pelo method get
 	@RequestMapping(method = RequestMethod.GET)
-	public String Listar() {
-		return "REST está funcionando";
+	public List<Categoria> Listar() {
+		Categoria cat1 = new Categoria(1, "informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> lista = new ArrayList<>();
+
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 
 }
