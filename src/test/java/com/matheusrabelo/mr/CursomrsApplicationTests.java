1d0d5ccb3477e27e4350bb395116e0c26b53689a
Matheus rabelo <matheusrabelo520@gmail.com>
2021-02-17 18:04:02 -0300

    projeto criado com sucesso

---
+++ src/test/java/com/matheusrabelo/mr/CursomrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusrabelo.mr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
