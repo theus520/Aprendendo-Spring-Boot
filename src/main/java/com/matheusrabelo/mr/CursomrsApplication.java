b49dcf5528c5c8dffcd179a755c0f97d2300779b
Matheus rabelo <matheusrabelo520@gmail.com>
2021-02-19 08:21:52 -0300

    Criando operacao de instaciacao

--- src/main/java/com/matheusrabelo/mr/CursomrsApplication.java
+++ src/main/java/com/matheusrabelo/mr/CursomrsApplication.java
@@ -1,13 +1,33 @@
 package com.matheusrabelo.mr;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matheusrabelo.mr.Repositories.CategoriaRepository;
+import com.matheusrabelo.mr.domain.Categoria;
+
 @SpringBootApplication
-public class CursomrsApplication {
+public class CursomrsApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomrsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+
 	}
 
 }
