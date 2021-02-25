0c2601f564bd93262d1b87d8920b90819d9076c5
Matheus rabelo <matheusrabelo520@gmail.com>
2021-02-24 21:35:42 -0300

    Atualizacao usando apenas JsonIgnore

--- src/main/java/com/matheusrabelo/mr/domain/Cliente.java
+++ src/main/java/com/matheusrabelo/mr/domain/Cliente.java
@@ -14,8 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.matheusrabelo.mr.domain.enums.TipoCliente;
 
 @Entity
@@ -32,7 +31,7 @@ public class Cliente implements Serializable {
 	private String cpfOucnpj;
 	private Integer tipo;
 
-	@JsonManagedReference
+	
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
@@ -40,7 +39,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name = "TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
  	
--- src/main/java/com/matheusrabelo/mr/domain/Pedido.java
+++ src/main/java/com/matheusrabelo/mr/domain/Pedido.java
@@ -15,8 +15,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 @Entity
 public class Pedido implements Serializable {
 
@@ -27,11 +25,10 @@ public class Pedido implements Serializable {
 	private Integer id;
 	private Date istance;
 
-	@JsonManagedReference
+	
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
